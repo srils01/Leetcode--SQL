c06566708342032a151212ea6dc0c03dc09e12f4
Srilekha Mylavarapu <114260250+srils01@users.noreply.github.com>
2024-01-17 13:52:40 -0600

    Create Q16.sql

---
+++ Basic Aggregate functions/Q16.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+select prices.product_id,ifnull(round(sum(prices.price*unitssold.units)/sum(units),2),0) as average_price
+from prices
+left join unitssold
+on prices.product_id=unitssold.product_id and
+unitssold.purchase_date between start_date and end_date
+group by product_id
