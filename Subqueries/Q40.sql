3eb8841832818d71f8a44a19bc40e40481ed204b
Srilekha Mylavarapu <114260250+srils01@users.noreply.github.com>
2024-01-17 14:17:11 -0600

    Create Q40.sql

---
+++ Subqueries/Q40.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select id , count(id) as num
+from ( select requester_id as id from requestaccepted
+    union all
+    select accepter_id from requestaccepted
+)derived_table
+group by id
+order by count(id) desc limit 1 
